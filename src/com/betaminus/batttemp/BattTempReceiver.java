cbc4a28d2d3d76f6331acfdc6936c510a69f7b40
Chris Rae <clr@chrisrae.com>
2015-09-17 14:48:31 -0700

    Not updating the value when the receiver is changed for other events which don't report battery state

--- src/com/betaminus/batttemp/BattTempReceiver.java
+++ src/com/betaminus/batttemp/BattTempReceiver.java
@@ -10,6 +10,11 @@ public class BattTempReceiver extends BroadcastReceiver {
 	@Override
 	public final void onReceive(Context context, Intent intent) {
         Log.d(BattTempPlugin.LOG_TAG, "Got battery temperature change");
-        BattTempPlugin.stateChanged(intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0), context);
+        int temp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -999);
+        // Sometimes this doesn't return anything (seems to be mainly when charging state changes, which
+        // triggers the broadcast but doesn't return anything for temperature. In those cases, just
+        // leave the previous value there.
+        if (temp != -999)
+        	BattTempPlugin.stateChanged(temp, context);
 	}
 }
